563e71690fec71b56828a186c0bb0ab9bf9a15f3
Alex Hornung <alex@alexhornung.com>
2012-02-18 16:42:35 +0000

    hdl - add sram sharing and connection modules
     * sram_bridge is basically a module with an internal Avalon-MM slave
       connection and an exported Avalon-MM master conduit.
    
     * The actual sram_bridge is not parameterized because Altera QSys
       cannot deal properly with parameterized data connections of Avalon-MM
       interfaces.
    
     * sram_arb is a module connecting 2 (or more) Avalon-MM master
       interfaces to an SRAM interface.

---
+++ hdl/ip/sram_bridge/sram_bridge_param.v
@@ -0,0 +1,47 @@
+module sram_bridge #(
+  parameter ADDR_WIDTH = 20,
+            DATA_WIDTH = 16,
+            BE_WIDTH = DATA_WIDTH/8
+)(
+  input                   clock,
+  input                   nreset,
+
+
+  /* Avalon MM slave interface */
+  input  [ADDR_WIDTH-1:0] address,
+  input  [  BE_WIDTH-1:0] byteenable,
+
+  input                   read,
+  output [DATA_WIDTH-1:0] readdata,
+
+  input                   write,
+  input  [DATA_WIDTH-1:0] writedata,
+
+  output                  waitrequest,
+
+
+  /* Avalon MM master interface */
+  output                  m_clock,
+  output [ADDR_WIDTH-1:0] m_address,
+  output [  BE_WIDTH-1:0] m_byteenable,
+
+  output                  m_read,
+  input  [DATA_WIDTH-1:0] m_readdata,
+
+  output                  m_write,
+  output [DATA_WIDTH-1:0] m_writedata,
+
+  input                   m_waitrequest
+);
+
+
+  assign m_clock      = clock;
+  assign m_address    = address;
+  assign m_byteenable = byteenable;
+  assign m_read       = read;
+  assign readdata     = m_readdata;
+  assign m_write      = write;
+  assign m_writedata  = writedata;
+  assign waitrequest  = ~m_waitrequest;
+
+endmodule
---
+++ hdl/sram_arb.v
@@ -0,0 +1,83 @@
+module sram_arb #(
+  parameter ADDR_WIDTH = 20,
+            DATA_WIDTH = 16,
+				SEL_WIDTH  = 1,
+				BE_WIDTH   = DATA_WIDTH/8
+)(
+  input                   clock,
+  input                   reset_n,
+
+
+  /* Master select */
+  input  [ SEL_WIDTH-1:0] sel,
+
+
+  /* SRAM interface */
+  output [ADDR_WIDTH-1:0] sram_address,
+  inout  [DATA_WIDTH-1:0] sram_data,
+  output                  sram_ce_n,  /* Chip enable        */
+  output                  sram_oe_n,  /* Output enable      */
+  output                  sram_we_n,  /* Write enable       */
+  output [  BE_WIDTH-1:0] sram_be_n,  /* Byte enable/select */
+
+
+  /* Avalon MM slave interface for sopc */
+  input  [ADDR_WIDTH-1:0] sopc_address,
+  input  [  BE_WIDTH-1:0] sopc_byteenable,
+
+  input                   sopc_read,
+  output [DATA_WIDTH-1:0] sopc_readdata,
+
+  input                   sopc_write,
+  input  [DATA_WIDTH-1:0] sopc_writedata,
+
+  output                  sopc_waitrequest,
+
+
+  /* Avalon MM slave interface for test runner */
+  input  [ADDR_WIDTH-1:0] tr_address,
+  input  [  BE_WIDTH-1:0] tr_byteenable,
+
+  input                   tr_read,
+  output [DATA_WIDTH-1:0] tr_readdata,
+
+  input                   tr_write,
+  input  [DATA_WIDTH-1:0] tr_writedata,
+
+  output                  tr_waitrequest
+);
+
+
+  /* Internal MUX'ed (arbitrated) Avalon-MM slave signals */
+  wire   [ADDR_WIDTH-1:0] address;
+  wire   [  BE_WIDTH-1:0] byteenable;
+  wire                    read;
+  wire   [DATA_WIDTH-1:0] readdata;
+  wire                    write;
+  wire   [DATA_WIDTH-1:0] writedata;
+
+
+
+  assign address          =  (sel == 1'b0) ? sopc_address    : tr_address;
+  assign byteenable       =  (sel == 1'b0) ? sopc_byteenable : tr_byteenable;
+  assign writedata        =  (sel == 1'b0) ? sopc_writedata  : tr_writedata;
+  assign read             =  (sel == 1'b0) ? sopc_read       : tr_read;
+  assign write            =  (sel == 1'b0) ? sopc_write      : tr_write;
+  assign sopc_waitrequest = ~(sel == 1'b0);
+  assign tr_waitrequest   = ~(sel == 1'b1);
+
+
+  assign sopc_readdata    = readdata;
+  assign tr_readdata      = readdata;
+
+
+  assign sram_ce_n     = 1'b0;
+  assign sram_oe_n     = (~read  || write);
+  assign sram_we_n     = (~write || read);
+  assign sram_be_n     = ~byteenable;
+
+
+  assign sram_address  = address;
+  assign sram_data     = (write && ~read ) ? writedata : DATA_WIDTH'bz;
+  assign readdata      = (read  && ~write) ? sram_data : DATA_WIDTH'b0;
+endmodule
