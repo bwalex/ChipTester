32124f938f76f55d86c2924249a5a6beac82680f
Alex Hornung <alex@alexhornung.com>
2012-03-04 17:29:53 +0000

    sim - reshuffle, move core hdl to hdl/ top folder

--- hdl/sim/sim_tester/load_sim.tcl
+++ hdl/sim/sim_tester/load_sim.tcl
@@ -1,5 +1,7 @@
 # Set hierarchy variables used in the Qsys-generated files
 set TOP_LEVEL_NAME "top"
+set HDL_BASE "../.."
+set SIM_BASE "$HDL_BASE/sim"
 
 proc ensure_lib { lib } { if ![file isdirectory $lib] { vlib $lib } }
 ensure_lib      ./libraries/     
@@ -7,14 +9,14 @@ ensure_lib      ./libraries/work/
 vmap work       ./libraries/work/
 
 # Compile the additional test files
-vlog -sv ./async_sram.v
-vlog     ./sram_arb_sync.v
-vlog     ./mem_if.v
-vlog     ./stim.v
-vlog     ./check.v
-vlog     ./cfifo.v
-vlog     ./rfifo.v
-vlog     ./stfifo.v
+vlog -sv $SIM_BASE/async_sram.v
+vlog     $HDL_BASE/sram_arb_sync.v
+vlog     $HDL_BASE/mem_if.v
+vlog     $HDL_BASE/stim.v
+vlog     $HDL_BASE/check.v
+vlog     $HDL_BASE/cfifo.v
+vlog     $HDL_BASE/rfifo.v
+vlog     $HDL_BASE/stfifo.v
 vlog     ./loopback.v
 vlog -sv ./top.sv
 # Elaborate the top-level design
