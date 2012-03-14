d96084f8a50fb216d2d4529b69ad1fea9dcd019b
Alex Hornung <alex@alexhornung.com>
2012-03-14 09:59:39 +0000

    bootstrap - change location of pre-bootstrap files
     * Don't integrate the pre-bootstrap files in the overlay or patchq as
       those files are tracked. The post-bootstrapped version of these files
       would then appear as changesets to the VCS.
    
     * Instead copy them into the overlay and/or patchq after bootstrapping
       and have the vanilla versions somewhere else, tracked.

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,6 +2,6 @@
 
 cwd=$(pwd)
 
-awk '{ sub(/CONFIG_DTB_SOURCE=.*/,"CONFIG_DTB_SOURCE=\"'$cwd'/hdl/linuxsys.dts\"");  print }' overlay/uClinux-dist/linux-2.6.x/.config > .tmp.config
+awk '{ sub(/CONFIG_DTB_SOURCE=.*/,"CONFIG_DTB_SOURCE=\"'$cwd'/hdl/linuxsys.dts\"");  print }' bootstrap/linux/.config > .tmp.config
 mv .tmp.config overlay/uClinux-dist/linux-2.6.x/.config
 
