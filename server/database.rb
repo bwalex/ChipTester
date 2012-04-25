ca79b4226a2785bc55155719c9a94c5075984dc3
Alex Hornung <ahornung@gmail.com>
2012-04-25 22:35:29 +0100

    server/database - new format of tables (WIP)
     * Completely overhaul the database tables to accomodate better all the
       new features and make it more flexible.
    
     * This is still work in progress and might change. In addition, the
       code around it has not been changed to work with the new table
       layout.

--- server/database.rb
+++ server/database.rb
@@ -5,38 +5,64 @@ DataMapper.setup(:default, 'mysql://root@localhost/ChipTester')
 #DataMapper.setup(:default, 'sqlite:test.db')
 DataMapper::Logger.new($stdout, :debug)
 
-class DUV_Descriptor
+class LogEntry
   include DataMapper::Resource
-  
-  property :id, Serial  # An auto-increment integer key
-  property :chip_number, Integer,:required => true # The number of the chip
-  property :team_number, Integer      # The number of the team
-  property :configure_date, DateTime  # A DateTime, for any date you might like.
+  property :id, Serial
+
+  property :type, Integer # 0 -> info, 1 -> warn, 2 -> error
+  property :message, String
+  property :file, String
 end
+
 
 class Result
   include DataMapper::Resource
-  property :id, Serial  # An auto-increment integer key
-  #property :id_test, Integer
-  property :chip_number, Integer, :required => true #The number of the chip
-  property :team_number, Integer 
-  property :test_passed, Boolean, :required => true #If the test passed or not
-  property :frequency, Integer
-  property :temperature, Integer
-  property :file_name, String #File name of the test
-
-  has n, :fails
+  property :id, Serial
+  property :team, Integer # 1
+  property :run_date, DateTime
+  property :academic_year, Integer # 2011/12
+  property :outcome, Integer # 0 -> pass, 1 -> fail
+  property :created_at, DateTime
+  property :updated_at, DateTime
+  property :virtual, Boolean # whether it's a virtual design or not
+  has n, :design_results
 end
 
-class Fail
+class DesignResult
   include DataMapper::Resource
   property :id, Serial
- # property :id_test, Integer
-  property :index, Integer
-  property :fail_result, String, :required => true
-  property :expected_result, String, :required => true
   belongs_to :result
+  property :outcome, Integer # 0 -> pass, 1 -> fail
+  property :run_date, DateTime
+  property :created_at, DateTime
+  property :updated_at, DateTime
+  property :file_name, String # team3/design1/foo.vec
+  property :clock_freq, String # 15 MHz
+  property :design_name, String # 4-bit Adder
+  has n, :test_vector_results
 end
+
+class TestVectorResult
+  include DataMapper::Resource
+  property :id, Serial
+  belongs_to :design_result
+  property :type, Integer # e.g. 0 -> triggered, 1 -> fixed latency, ...
+  property :input_vector, String # 1101000001
+  property :expected_result, String # 1111010101
+  property :actual_result, String # 10110101010
+  property :cycle_count, Integer # how many cycles the execution took
+  property :clock_freq, String # 15 MHz
+  property :clock_connections, String # e.g. A1, A2, A23
+  property :bitmask, String # Q1,Q2,Q4
+  property :dontcare_mask, String # not implemented hardware-wise yet, but e.g. Q1, Q3, Q5
+  property :trigger_mask, String # Q1 or Q2 or !Q5
+  property :outcome, Integer # 0 -> pass, 1 -> fail
+  property :trigger_timeout, Boolean
+  property :has_run, Boolean
+  property :created_at, DateTime
+  property :updated_at, DateTime
+end
+
 
 def Store_DUV_Result(json_parsed)
       @duv_result = Result.create(
@@ -60,4 +86,4 @@ def Store_DUV_Fail(json_parsed)
 end
 DataMapper.finalize
 #DataMapper.auto_migrate!
-DataMapper.auto_upgrade!
+#DataMapper.auto_upgrade!
